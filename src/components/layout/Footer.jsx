193ad8cafe2ef4ea9d3120589155ee47b0091331
Romain <romdev34@gmail.com>
2024-09-09 01:36:58 +0200

    misc

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer className="block m-auto text-justify  w-[700px]"><ul className="  ">
-            <li>Vous pouvez créer un rendez vous en cliquant sur le jour au niveau du de la vue "mois"</li>
-            <li>{`Vous pouvez créer 1 note journalière pour la durée de votre choix qui en selectionnant type de rdv => note`}</li>
+            <li>Pour créer un rendez vous, se rendre sur la vue "mois" puis cliquer sur le jour souhaité</li>
+            <li>{`Vous pouvez créer 1 note pour le ou les jours concernés en créant un évenement de type note`}</li>
             <li>Les notes apparaissent en haut du calendrier dans la vue "Semaine" et "Jour"</li>
         </ul></footer>
     )
